c91eaadfccc57ac949acce229e2405d4df3107f8
ioannis-sporidis <ioannis.sporidis@gmail.com>
2021-08-18 10:47:51 -0400

    Fetching API data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+import { useState, useEffect } from 'react';
+import { FaSearch } from 'react-icons/fa';
+import Photo from './Photo';
+// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
+const mainURL = `https://api.unsplash.com/photos/`;
+const searchURL = `https://api.unsplash.com/search/photos/`;
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
+
 function App() {
-  return <div className='App'></div>;
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState([]);
+
+  const fetchImages = async () => {
+    setLoading(true);
+    let url;
+    url = `${mainURL}${clientID}`;
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchImages();
+  }, []);
+
+  return <h2>stock photos starter</h2>;
 }
 
 export default App;
